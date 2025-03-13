37efef9b8922d6cd90cfa4a03005ff73ad6041e2
Benoit Beaulieu <benoit-beaulieu82@hotmail.com>
2025-03-13 17:23:08 -0400

    guudguud

--- main/Sim_temperature/untitled.m
+++ main/Sim_temperature/untitled.m
@@ -3,4 +3,4 @@ clear
 %fct_simuler_plaque("param.json")
 %sim_with_snapshots("param.json")
 %Copy_of_sim_with_snapshots("param.json")
-new_sim_with_snapshots("param.json")
+%new_sim_with_snapshots("param.json")
