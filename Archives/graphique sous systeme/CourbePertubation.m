6a51003a5019f15611193014edd53ae4db2c058e
Valérie Duclos-Phan <valerie.duclos-phan.1@ulaval.ca>
2025-04-11 17:32:53 -0400

    pertubation

--- Archives/graphique sous systeme/CourbePertubation.m
+++ Archives/graphique sous systeme/CourbePertubation.m
@@ -6,21 +6,28 @@ T2 = data.signals.values(:,2);
 T1 = data.signals.values(:,1);
 
 for i = 1:size(T1,1)
-    T1(i) = T1(i) + 25;
+    T1(i) = T1(i) + 24;
 end
 
 for i = 1: size(T2,1)
-    T2(i) = T2(i) + 25;
+    T2(i) = T2(i) + 24;
 end
 
 for i = 1: size(T3,1)
-    T3(i) = T3(i) + 25;
+    T3(i) = T3(i) + 24;
 end
 
 hold on
+subplot(3,1,[1 2])
 plot(time,T1,'-','LineWidth', 3);
 plot(time,T2,'--','LineWidth', 3);
 plot(time,T3,'-.','LineWidth', 3);
 ylabel('Température [^oC]');
 xlabel('Temps [s]')
-legend('T1','T2','T3','Location','southeast')
+
+plot(perturbationsOpen(2:end, 1), perturbationsOpen(2:end, 4),'o', 'LineWidth', 1.5);
+plot(perturbationsOpen(2:end, 1), perturbationsOpen(2:end, 5),'s','LineWidth', 1.5);
+plot(perturbationsOpen(2:end, 1), perturbationsOpen(2:end, 6),'^','LineWidth', 1.5);
+legend('T1 (simulation)','T2 (simulation)','T3 (simulation)','T1 (prototype)','T2 (prototype)','T3 (prototype)','Location','southeast')
+subplot(3,1,3)
+plot()
--- Archives/graphique sous systeme/PWMFiltreRC.m
+++ Archives/graphique sous systeme/PWMFiltreRC.m
@@ -1,20 +1,33 @@
 data = evalin('base','out');
-info = data.filtrePWM;
-temps = info.time(:,1);
+info = data.PWMsortie;
+temps = info(:,1);
 
 %pwm = info.signals(1).values(:,1);
-filtre = info.signals.values(:,1);
-subplot(3,1,[1 2]);
+pwm = info(:,2);
+subplot(2,1,1);
+
+% 1. Tronquer axeY à 1000 échantillons
+%axeY = axeY(1:1000);
+ 
+% 2. Tronquer filtre à 250 échantillons
+%pwm = pwm(1:250);
+%temps = temps(1:250);
+
+
+
+% 
 hold on
 %plot(temps, pwm,'LineWidth',1)
-plot(temps, filtre,'--', 'LineWidth',3)
-xlabel('Temps [s]')
+plot(temps, pwm,'--', 'LineWidth',3)
 ylabel('Tension [V]')
-%figure ;
-%hold on;
-axeX = moku.data(86800:87800, 1)-12.4;
-axeY = moku.data(86800:87800, 3) -1;
-plot(axeX, axeY,'LineWidth',3, 'Color','r');
-ylim([0.25 3.2])
-legend('filtre simulé','filtre mesuré','Location','southeast')
+xlabel('Temps [s]')
+legend('PWM simulé','Location','southeast')
+axeX = moku.data(86800:86900, 1)-12.4;
+axeY = moku.data(86800:86900, 2) -1;
 
+subplot(2,1,2);
+plot(axeX, axeY,'LineWidth',2, 'Color','r');
+xlim([0.00160799,5e-03]);
+ylabel('Tension [V]')
+xlabel('Temps [s]')
+legend('PWM mesuré','Location','southeast')
